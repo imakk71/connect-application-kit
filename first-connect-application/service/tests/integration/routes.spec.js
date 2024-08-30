5df91661dfdb25cecb67aa1b3c080374c15dbdb4
imakk71 <ishan@makkpress.com>
2024-08-30 16:04:49 +0530

    Remove the old-files directory

--- first-connect-application/service/tests/integration/routes.spec.js
+++
@@ -1,8 +0,0 @@
-import { expect } from '@jest/globals';
-
-//@todo: implement tests (use msw in shared package)
-describe('Testing Cart Controller', () => {
-  test('POST `/service` route', async () => {
-    expect(1).toBe(1);
-  });
-});
