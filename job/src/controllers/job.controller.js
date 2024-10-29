a975404a49e8ca8f8065efd4be4f3c7c2dd7bdb0
imakk71 <163394877+imakk71@users.noreply.github.com>
2024-10-29 12:01:24 +0530

    Update job.controller.js

--- job/src/controllers/job.controller.js
+++ job/src/controllers/job.controller.js
@@ -1,28 +1,16 @@
-import CustomError from '../errors/custom.error.js';
-import { logger } from '../utils/logger.utils.js';
-import { allOrders } from '../orders/fetch.orders.js';
+import { allCustomers } from '../customers/fetch.customers.js'; // Import your customer fetching function
 
-/**
- * Exposed job endpoint.
- *
- * @typedef {import("express").Response} Response
- * @typedef {import("express").Request} Request
- *
- * @param {Request} _request The express request
- * @param {Response} response The express response
- * @returns
- */
 export const post = async (_request, response) => {
   try {
-    // Get the orders
-    const limitedOrdersObject = await allOrders({ sort: ['lastModifiedAt'] });
-    logger.info(`There are ${limitedOrdersObject.total} orders!`);
+    const limitedCustomersObject = await allCustomers(); // Fetch customer details
+    logger.info(`There are ${limitedCustomersObject.total} customers!`);
 
-    response.status(200).send();
+    response.status(200).send(limitedCustomersObject); // Return customer details
   } catch (error) {
+    logger.error(`Error: ${error.message}`);
     throw new CustomError(
       500,
-      `Internal Server Error - Error retrieving all orders from the commercetools SDK`
+      `Internal Server Error - Error retrieving all customers from the commercetools SDK`
     );
   }
 };
