7366b2ac70622497f1af8fe5b79a318a034f7de3
imakk71 <163394877+imakk71@users.noreply.github.com>
2024-10-29 11:05:01 +0530

    Update custom-application-config.mjs

--- merchant-center-custom-application/custom-application-config.mjs
+++ merchant-center-custom-application/custom-application-config.mjs
@@ -1,9 +1,34 @@
-export default {
-  entryPointUriPath: "website-speedy-optimization",
-  cloudIdentifier: "gcp-eu",
-  oAuthScopes: ["view_customers"], 
+const config = {
+  name: 'Website Speedy',
+  entryPointUriPath: '${env:website-speedy-optimization}',
+  cloudIdentifier: '${env:CLOUD_IDENTIFIER}',
   env: {
-    applicationId: "cm2ry5jvs0003kpl9n1p3s0sd",
-    url: "https://app.websitespeedy.com",
-  }
+    development: {
+      initialProjectKey: 'website-speedy-optimization',
+    },
+    production: {
+      applicationId: '${env:cm2ry5jvs0003kpl9n1p3s0sd}',
+      url: '${env:website-speedy-optimization}',
+    },
+  },
+  oAuthScopes: {
+    view: ['view_customers'],
+    manage: ['manage_products'],
+  },
+  icon: '${path:@commercetools-frontend/assets/application-icons/rocket.svg}',
+  mainMenuLink: {
+    defaultLabel: 'Template starter',
+    labelAllLocales: [],
+    permissions: [PERMISSIONS.View],
+  },
+  submenuLinks: [
+    {
+      uriPath: 'channels',
+      defaultLabel: 'Channels',
+      labelAllLocales: [],
+      permissions: [PERMISSIONS.View],
+    },
+  ],
 };
+
+export default config;
