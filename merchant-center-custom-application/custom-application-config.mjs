800af65d98ee4ccf68b99f1067d459aa53deb41b
imakk71 <163394877+imakk71@users.noreply.github.com>
2024-10-29 10:40:32 +0530

    Create custom-application-config.mjs

---
+++ merchant-center-custom-application/custom-application-config.mjs
@@ -0,0 +1,9 @@
+export default {
+  entryPointUriPath: "website-speedy-optimization",
+  cloudIdentifier: "gcp-eu",
+  oAuthScopes: ["view_customers"], 
+  env: {
+    applicationId: "cm2ry5jvs0003kpl9n1p3s0sd",
+    url: "https://app.websitespeedy.com",
+  }
+};
